8cc7ccf1ba6b84d8e48fcaa52a11581b92bfd354
Alan Leverenz <awleverenz@aol.com>
2019-10-02 02:44:44 -0400

    edited to include some hidden files

---
+++ keys.js
@@ -0,0 +1,16 @@
+console.log('this is loaded');
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  };
+
+// OMDB
+exports.omdbKeys = {
+    id: '22499961'
+}
+
+// BANDS IN TOWN
+exports.bandintownKeys = { 
+    id: 'codingbootcamp'
+}
